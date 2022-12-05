544129c883062b2065cb3cf044108dc0721aae01
rubentanoey <92169008+rubentanoey@users.noreply.github.com>
2022-12-06 00:32:21 +0700

    Update Admin Dash

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from dashboard.views import dashboard_kurir, dashboard_pelanggan, dashboard_restoran
+from dashboard.views import dashboard_kurir, dashboard_pelanggan, dashboard_restoran, dashboard_admin
 
 app_name = "dashboard"
 
 urlpatterns = [
+    path("admin/<int:pk>/", dashboard_admin, name = "dashboard_admin"),
     path("pelanggan/<int:pk>/", dashboard_pelanggan, name = "dashboard_pelanggan"),
     path("restoran/<int:pk>/", dashboard_restoran, name = "dashboard_resto"),
     path("kurir/<int:pk>/", dashboard_kurir, name = "dashboard_kurir"),
