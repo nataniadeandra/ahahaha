7ffd191c33da7dd2fb61c4cc62c7978fbce647a9
rubentanoey <92169008+rubentanoey@users.noreply.github.com>
2022-12-10 09:40:35 +0700

    DONE 70% (sigh)

--- trigger_4/urls.py
+++ trigger_4/urls.py
@@ -6,15 +6,13 @@ app_name = 'trigger_4'
 urlpatterns = [
     # Kategori Makanan
     path('form-kategori-makanan/', views.create_kategori_makanan, name='form-kategori-makanan'),
-    path('kategori-makanan-list/', views.read_kategori_makanan, name='kategori-makanan-list'),
-    path('kategori-makanan-list/hapus/<id>', views.read_kategori_makanan, name='kategori-makanan-list'),
-
+    path('list-kategori-makanan/', views.read_kategori_makanan, name='list-kategori-makanan'),
+    path('hapus-kategori-makanan/<str:id>/', views.delete_kategori_makanan, name='hapus-kategori-makanan'),
+    
+    # Pesan Makanan (POV Pelanggan)
     path('form-alamat-pesanan/', views.create_alamat_pesanan, name='form-alamat-pesanan'),
-    path('daftar-pesanan/', views.daftar_pesanan, name='daftar-pesanan'),
-    path('form-alamat-pesanan/', views.create_alamat_pesanan, name='form-alamat-pesanan'),
+    path('list-resto/<str:province>/', views.pilih_restolist_pesanan, name='list-resto'),
+    path('list-menu/<str:rname>/<str:rbranch>/', views.pilih_restomenu_pesanan, name='list-menu'),
+    path('list-pesanan/', views.list_pesanan, name='list-pesanan'),
     path('konfirmasi-pembayaran/', views.konfirmasi_pembayaran, name='konfirmasi-pembayaran'),
-    path('pemilihan-detail-pesanan/', views.pemilihan_detail_pesanan, name='pemilihan-detail-pesanan'),
-    path('pemilihan-restoran/', views.pemilihan_restoran, name='pemilihan-restoran'),
-    path('pesanan-berlangsung/', views.pesanan_berlangsung, name='pesanan-berlangsung'),
-    path('ringkasan-pesanan/', views.ringkasan_pesanan, name='ringkasan-pesanan'), 
 ]
