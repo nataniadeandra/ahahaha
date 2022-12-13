2058594f11e97ea80fe4f6f95c8b5405dcfe41b6
Natania Deandra <nataniadeandra@gmail.com>
2022-12-13 21:23:38 +0700

    dep

--- sirest/settings.py
+++ sirest/settings.py
@@ -133,3 +133,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://sirest-django-app-production.up.railway.app",
+    "https://sirest-django-app-production-fc5d.up.railway.app",
+    r"https://containers-us-west-141.railway.app",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    ]
