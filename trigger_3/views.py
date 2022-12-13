a0378db9450f4fcc4919628486d712d34531d9db
Muhammad Faras Baginda <muhammad.faras@ui.ac.id>
2022-12-13 21:15:46 +0700

    Trigger 3 65%

--- trigger_3/views.py
+++ trigger_3/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from sirest.utils import *
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from sirest.utils import get_role, is_authenticated
 from django.db import connection
+from django.urls import reverse
 
 # Create your views here.
 def namedtuplefetchall(cursor):
@@ -28,8 +29,14 @@ def makanan_page(request):
     cursor.execute("SET SEARCH_PATH TO SIREST")
     cursor.execute('SELECT * FROM FOOD INNER JOIN FOOD_CATEGORY ON FOOD.FCATEGORY = FOOD_CATEGORY.id')
     hasil = namedtuplefetchall(cursor)
-    print (hasil)
     return render(request, 'makanan.html', {'hasil':hasil})
+
+def delete_makanan(request, rname, rbranch, foodname): 
+    get_query(f"""
+        DELETE FROM sirest.FOOD 
+        WHERE rname = '{rname}' AND rbranch = '{rbranch}' AND foodname = '{foodname}';
+    """)
+    return redirect('/trigger_3/makanan/')
 
 def restoran_detail_makanan(request):
     return render(request, 'restoran_makanan_detail.html')
@@ -41,10 +48,22 @@ def restoran_makanan_page(request):
     return render(request, 'restoran_makanan.html')
 
 def tarif_pengiriman_page(request):
-    return render(request, 'tarif_pengiriman.html')
+    cursor = connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO SIREST")
+    cursor.execute('SELECT * FROM DELIVERY_FEE_PER_KM')
+    hasil = namedtuplefetchall(cursor)
+    return render(request, 'tarif_pengiriman.html', {'hasil':hasil})
 
 def tambah_tarif_pengiriman(request):
     return render(request, 'tarif_pengiriman_add.html')
 
 def update_tarif_pengiriman(request):
     return render(request, 'tarif_pengiriman_update.html')
+
+def delete_tarif_pengiriman(request, id): 
+    get_query(f"""
+        DELETE FROM sirest.DELIVERY_FEE_PER_KM 
+        WHERE id = '{id}';  
+    """)
+
+    return redirect('/trigger_3/tarif_pengiriman/')
